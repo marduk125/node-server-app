ac8776754bfa476510106c62cbe145fcc033a9d8
Mahdi Rahmen <mahdi.rahmen@opencode.com>
2020-04-12 10:58:51 +0100

    add project page

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.get('/about',(req,res)=>{
         pageTitle:'About Page'
     })
 })
+app.get('/project',(req,res)=>{
+    res.render('project.hbs',{
+        pageTitle:'Project Page'
+    })
+})
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage:'Unable to find page'
